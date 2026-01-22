9fa8422c93a874aa613ffcab208eed11c3f7f88d
Berk Memiş <berk.memis@sono.com.tr>
2026-01-22 15:32:27 +0300

    refactor: move modules to 'modules' directory and flatten project structure

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,6 +1,13 @@
 rootProject.name = "meal-recommendation-platform"
 
 include("01-infrastructure")
+project(":01-infrastructure").projectDir = file("modules/01-infrastructure")
+
 include("02-domain")
+project(":02-domain").projectDir = file("modules/02-domain")
+
 include("03-application")
+project(":03-application").projectDir = file("modules/03-application")
+
 include("04-utilities")
+project(":04-utilities").projectDir = file("modules/04-utilities")
